e1f3b58b9cd02708852ddc5cdc3985705a648b3e
Humzah Al-Kindi <humzahalkindi@hotmail.com>
2024-06-09 10:11:09 +0000

    Secure code sample created using copilot that checks data type but doesn't address language vularnabilities

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -16,16 +16,24 @@ from collections import namedtuple
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
+VALID_ITEM_TYPES = {'payment', 'product'}
+
 def validorder(order: Order):
+    if not isinstance(order, Order):
+        return "Invalid order: %s" % order
+
     net = 0
 
     for item in order.items:
+        if not isinstance(item, Item):
+            return "Invalid item: %s" % item
+        if item.type not in VALID_ITEM_TYPES:
+            return "Invalid item type: %s" % item.type
+
         if item.type == 'payment':
             net += item.amount
         elif item.type == 'product':
             net -= item.amount * item.quantity
-        else:
-            return "Invalid item type: %s" % item.type
 
     if net != 0:
         return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
